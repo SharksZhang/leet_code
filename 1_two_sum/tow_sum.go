d0663d19da0e7e6f8f3210a93bd2cd444e9900b1
believein08 <believein08@163.com>
2017-12-24 17:05:29 +0800

    finish the first question two sum

---
+++ 1_two_sum/tow_sum.go
@@ -0,0 +1,22 @@
+package __two_sum
+
+/*Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].*/
+func twoSum(nums []int, target int) []int {
+	temp := make(map[int]int)
+	for index, value := range(nums){
+		if result,ok := temp[target-value]; ok{
+			return []int{result,index}
+		} else{
+			temp[value] = index
+		}
+	}
+	return nil
+}
