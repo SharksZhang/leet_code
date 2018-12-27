9da4c30547b72bd7fc52607474b9a564f8a2120e
SharksZhang <2425194544@qq.com>
2018-12-27 19:05:01 +0800

    complete longest_palindromic_substring_5 with tdd

--- longest_palindromic_substring_5/solution.py
+++ longest_palindromic_substring_5/solution.py
@@ -21,18 +21,31 @@ class Solution:
         :type s: str
         :rtype: str
         """
+        longest_palindrome = ""
+        for i in range(0, len(s)):
+            palindrome = self.longest_palindrome_with_index(s, i)
+            if len(longest_palindrome) < len(palindrome):
+                longest_palindrome = palindrome
+        return longest_palindrome
     def longest_palindrome_with_index(self, s, index):
         if s == "":
             return ""
-        return self.longest_single_mid_palindrome_with_index(index, s)
+        single_mid = self.longest_single_mid_palindrome_with_index(s, index)
+        double_mid = self.longest_double_mid_palindrome_with_index(s, index)
+        return  single_mid if len(single_mid) > len(double_mid) else  double_mid
 
-    def longest_single_mid_palindrome_with_index(self, index, s):
+    def longest_single_mid_palindrome_with_index(self, s, index):
         i = 0
         for i in range(1, index + 1):
-            if index - i < 0 or index + i == len(s) or s[index - i] != s[index + i]:
+            if index - i < 0 or index + i >= len(s) or s[index - i] != s[index + i]:
                 i = i - 1
                 break
         return s[index - i:index + i + 1]
 
     def longest_double_mid_palindrome_with_index(self, s, index):
-        return s[0:1]
+        i = 0
+        for i in range(0, index + 1):
+            if index - i < 0 or index + i +1 >= len(s) or s[index - i] != s[index + i + 1]:
+                i = i - 1
+                break
+        return s[index - i : index + i + 2]
--- longest_palindromic_substring_5/solution_test.py
+++ longest_palindromic_substring_5/solution_test.py
@@ -35,12 +35,26 @@ class TestSolution(unittest.TestCase):
         res = Solution().longest_palindrome_with_index(s, index)
         self.assertEqual(res_expect, res)
 
-    # def test_is_palindrome_with_index_abbc(self):
-    #     s = "abbc"
-    #     index = 1
-    #     resExpect = "abbc"
-    #     res = Solution().longest_palindrome_with_index(s, index)
-    #     self.assertEqual(resExpect, res)
+    def test_is_palindrome_with_index_cbbc_index0(self):
+        s = "cbbc"
+        index = 0
+        res_expect = "c"
+        res = Solution().longest_palindrome_with_index(s, index)
+        self.assertEqual(res_expect, res)
+
+    def test_is_palindrome_with_index_abbc(self):
+        s = "abbc"
+        index = 1
+        resExpect = "bb"
+        res = Solution().longest_palindrome_with_index(s, index)
+        self.assertEqual(resExpect, res)
+
+    def test_is_palindrome_with_index_bbbbb(self):
+        s = "bbbbb"
+        index = 2
+        resExpect = "bbbbb"
+        res = Solution().longest_palindrome_with_index(s, index)
+        self.assertEqual(resExpect, res)
 
     def test_longest_double_mid_palindrome_with_index(self):
         s = "bb"
@@ -48,3 +62,30 @@ class TestSolution(unittest.TestCase):
         res_expect = "bb"
         res = Solution().longest_double_mid_palindrome_with_index(s, index)
         self.assertEqual(res_expect, res)
+
+    def test_longest_double_mid_palindrome_with_index_abba(self):
+        s = "abba"
+        index = 0
+        res_expect = ""
+        res = Solution().longest_double_mid_palindrome_with_index(s, index)
+        self.assertEqual(res_expect, res)
+
+    def test_longest_double_mid_palindrome_with_index_abba_index1(self):
+        s = "abba"
+        index = 1
+        res_expect = "abba"
+        res = Solution().longest_double_mid_palindrome_with_index(s, index)
+        self.assertEqual(res_expect, res)
+
+    def test_longestPalindrome_normal(self):
+        s = "babadbda"
+        res_expect = "adbda"
+        res = Solution().longestPalindrome(s)
+        self.assertEqual(res_expect, res)
+
+    def test_longestPalindrome_double_mid(self):
+        s = "babadbbda"
+        res_expect = "adbbda"
+        res = Solution().longestPalindrome(s)
+        self.assertEqual(res_expect, res)
+
