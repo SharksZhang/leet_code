f427946856f9f1b7e0b936850307998f05594723
ericzhang08 <yuzhangfish@163.com>
2019-07-23 14:16:10 -0400

    add maxinumsubarray

--- java/src/maximunsubarray/Solution.java
+++ java/src/maximunsubarray/Solution.java
@@ -1,8 +1,14 @@
 package maximunsubarray;
 
-//class Solution {
-//    public int maxSubArray(int[] nums) {
-//
-//        return  result;
-//    }
-//}
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // DP(i) = max(Dp(i-1), num[i])
+        int result = nums[0];
+        int subSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            subSum = Math.max(subSum + nums[i], nums[i]);
+            result = Math.max(result, subSum);
+        }
+        return  result;
+    }
+}
